26fc130acaf019fee7827735d7a20c182bd86f71
masoud89git config --global user.email m.lakestani89@gmail.com <m.lakestani89@gmail.comgit config --global user.name masoud89>
2024-06-15 18:13:55 +0330

    fux a bug in adding cors middleware

--- config.py
+++ config.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI
 from routers import users, article
-
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Specify domains of clients allowed to access, or use '*' for open access
+    allow_credentials=True,
+    allow_methods=["*"],  # Specify which methods can be used to access the resource, or use '*' for all
+    allow_headers=["*"],  # Specify which headers can be used when making a request
+)
 app.include_router(users.router)
 app.include_router(article.router)
