c7cbd72fabfbe2c25a381c9bbe23ad73949be735
masoud89git config --global user.email m.lakestani89@gmail.com <m.lakestani89@gmail.comgit config --global user.name masoud89>
2024-06-16 11:29:47 +0330

    comment register

--- routers/__init__.py
+++ routers/__init__.py
@@ -1,2 +1,2 @@
 from .article import get_article
-from .users import create_user
+# from .users import create_user
--- routers/users.py
+++ routers/users.py
@@ -7,13 +7,13 @@ from painless.dependencies import get_db, get_register_key
 
 router = APIRouter()
 
-@router.post('/register', response_model=schemas.User)
-def create_user(user:schemas.UserCreate, db:Session=Depends(get_db), register_key:str=Depends(get_register_key)):
-    db_user = db.query(models.User).filter(models.User.email==user.email).first()
-    if db_user:
-        raise HTTPException(status_code=400, detail="The username already exist")
-    user = models.User(email=user.email, username=user.username, password=user.password)
-    db.add(user)
-    db.commit()
-    db.refresh(user)
-    return user
+# @router.post('/register', response_model=schemas.User)
+# def create_user(user:schemas.UserCreate, db:Session=Depends(get_db), register_key:str=Depends(get_register_key)):
+#     db_user = db.query(models.User).filter(models.User.email==user.email).first()
+#     if db_user:
+#         raise HTTPException(status_code=400, detail="The username already exist")
+#     user = models.User(email=user.email, username=user.username, password=user.password)
+#     db.add(user)
+#     db.commit()
+#     db.refresh(user)
+#     return user
