8ad9f97a0377c8250636622894dc6c59f88ecaf6
masoud89git config --global user.email m.lakestani89@gmail.com <m.lakestani89@gmail.comgit config --global user.name masoud89>
2024-06-22 12:17:10 +0330

    add updating alembic.ini file to core

--- db/core.py
+++ db/core.py
@@ -1,10 +1,22 @@
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+import configparser
 from settings import config
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
 
 
 SQLALCHEMY_DATABASE_URL = f"postgresql://{config['database']['DB_USER']}:{config['database']['DB_PASSWORD']}@{config['database']['DB_HOST']}:{config['database']['DB_PORT']}/{config['database']['DB_NAME']}"
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine)
 Base = declarative_base()
+
+
+
+ini_config = configparser.ConfigParser()
+
+ini_config.read('alembic.ini')
+
+ini_config['alembic']['sqlalchemy.url'] = SQLALCHEMY_DATABASE_URL
+
+with open('alembic.ini', 'w') as configfile:
+    ini_config.write(configfile)
